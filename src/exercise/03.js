a1aa7b7238f74df984d2e15bfee3f3776fc78830
Matthew Bender <matthew-bender@cirrusmd.com>
2021-03-05 14:33:56 -0700

    exercise 3 - extra credit 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -56,7 +56,22 @@ function ListItem({
     />
   )
 }
-ListItem = React.memo(ListItem)
+ListItem = React.memo(ListItem, (prevProps, nextProps) => {
+  // true means do NOT rerender
+  // false means DO rerender
+  if (prevProps.getItemProps !== nextProps.getItemProps) return false
+  if (prevProps.item !== nextProps.item) return false
+  if (prevProps.index !== nextProps.index) return false
+  if (prevProps.selectedItem !== nextProps.selectedItem) return false
+
+  if (prevProps.highlightedIndex !== nextProps.highlightedIndex) {
+    const wasPrevHighlighted = prevProps.highlightedIndex === prevProps.index
+    const isNowHighlighted = nextProps.highlightedIndex === nextProps.index
+    return wasPrevHighlighted === isNowHighlighted
+  }
+
+  return true
+})
 
 function App() {
   const forceRerender = useForceRerender()
