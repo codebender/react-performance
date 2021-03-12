e00fda0bf1ece201b3199560e456bad0019fb25b
Matthew Bender <matthew-bender@cirrusmd.com>
2021-03-12 16:45:09 -0700

    exercise 6- extra credit 4

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,5 +1,6 @@
-// Fix "perf death by a thousand cuts"
-// http://localhost:3000/isolated/exercise/06.js
+// Starting point for the Recoil Extra Credit
+// 💯 use recoil (exercise)
+// http://localhost:3000/isolated/exercise/06.extra-4.js
 
 import * as React from 'react'
 import {
@@ -9,21 +10,35 @@ import {
   updateGridState,
   updateGridCellState,
 } from '../utils'
+import {RecoilRoot, useRecoilState, useRecoilCallback, atomFamily} from 'recoil'
 
 const AppStateContext = React.createContext()
-const AppDispatchContext = React.createContext()
 
 const initialGrid = Array.from({length: 100}, () =>
   Array.from({length: 100}, () => Math.random() * 100),
 )
 
+const cellAtoms = atomFamily({
+  ket: 'cells',
+  default: ({row, column}) => initialGrid[row][column],
+})
+
+function useUpdateGrid() {
+  return useRecoilCallback(({set}) => ({rows, columns}) => {
+    for (let row = 0; row < rows; row++) {
+      for (let column = 0; column < columns; column++) {
+        if (Math.random() > 0.7) {
+          set(cellAtoms({row, column}), Math.random() * 100)
+        }
+      }
+    }
+  })
+}
+
 function appReducer(state, action) {
   switch (action.type) {
-    case 'UPDATE_GRID_CELL': {
-      return {...state, grid: updateGridCellState(state.grid, action)}
-    }
-    case 'UPDATE_GRID': {
-      return {...state, grid: updateGridState(state.grid)}
+    case 'TYPED_IN_DOG_INPUT': {
+      return {...state, dogName: action.dogName}
     }
     default: {
       throw new Error(`Unhandled action type: ${action.type}`)
@@ -33,13 +48,12 @@ function appReducer(state, action) {
 
 function AppProvider({children}) {
   const [state, dispatch] = React.useReducer(appReducer, {
-    grid: initialGrid,
+    dogName: '',
   })
+  const value = [state, dispatch]
   return (
-    <AppStateContext.Provider value={state}>
-      <AppDispatchContext.Provider value={dispatch}>
-        {children}
-      </AppDispatchContext.Provider>
+    <AppStateContext.Provider value={value}>
+      {children}
     </AppStateContext.Provider>
   )
 }
@@ -52,19 +66,11 @@ function useAppState() {
   return context
 }
 
-function useAppDispatch() {
-  const context = React.useContext(AppDispatchContext)
-  if (!context) {
-    throw new Error('useAppDispatch must be used within the AppProvider')
-  }
-  return context
-}
-
 function Grid() {
-  const dispatch = useAppDispatch()
+  const updateGrid = useUpdateGrid()
   const [rows, setRows] = useDebouncedState(50)
   const [columns, setColumns] = useDebouncedState(50)
-  const updateGridData = () => dispatch({type: 'UPDATE_GRID'})
+  const updateGridData = () => updateGrid(rows, columns)
   return (
     <AppGrid
       onUpdateGrid={updateGridData}
@@ -76,21 +82,11 @@ function Grid() {
     />
   )
 }
-Grid = React.memo(Grid)
 
-function withStateSlice(Comp, slice) {
-  const MemoComp = React.memo(Comp)
-  function Wrapper(props, ref) {
-    const state = useAppState()
-    return <MemoComp ref={ref} state={slice(state, props)} {...props} />
-  }
-  Wrapper.displayName = `withStateSlice(${Comp.displayName || Comp.name})`
-  return React.memo(React.forwardRef(Wrapper))
-}
+function Cell({row, column}) {
+  const [cell, setCell] = useRecoilState(cellAtoms({row, column}))
+  const handleClick = () => setCell(Math.random() * 100)
 
-function Cell({state: cell, row, column}) {
-  const dispatch = useAppDispatch()
-  const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
     <button
       className="cell"
@@ -104,14 +100,14 @@ function Cell({state: cell, row, column}) {
     </button>
   )
 }
-Cell = withStateSlice(Cell, (state, {row, column}) => state.grid[row][column])
 
 function DogNameInput() {
-  const [dogName, setDogName] = React.useState('')
+  const [state, dispatch] = useAppState()
+  const {dogName} = state
 
   function handleChange(event) {
     const newDogName = event.target.value
-    setDogName(newDogName)
+    dispatch({type: 'TYPED_IN_DOG_INPUT', dogName: newDogName})
   }
 
   return (
@@ -136,12 +132,14 @@ function App() {
   return (
     <div className="grid-app">
       <button onClick={forceRerender}>force rerender</button>
-      <AppProvider>
-        <div>
-          <DogNameInput />
-          <Grid />
-        </div>
-      </AppProvider>
+      <RecoilRoot>
+        <AppProvider>
+          <div>
+            <DogNameInput />
+            <Grid />
+          </div>
+        </AppProvider>
+      </RecoilRoot>
     </div>
   )
 }
